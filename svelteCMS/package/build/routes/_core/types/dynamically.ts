7d0a15dbffe4ba35c6e5659bfef341f8c0a0838d
Anthony <anthonytoribio809@gmail.com>
2023-01-12 16:45:58 -0500

    allow multiple images and other updates

--- svelteCMS/package/build/routes/_core/types/dynamically.ts
+++ svelteCMS/package/build/routes/_core/types/dynamically.ts
@@ -1,30 +1,22 @@
-/** Object data for objects inside route:test */
-export type TestObjectData = {
-    test:string
-    _id:import("mongodb").ObjectId
-    _status:import("$Types").StatusData
-    _createdAt:number
-    _updatedAt:number
-    _categories:import("$Types").CategoryData[]
-    _tags:import("$Types").TagData[]
-}
-
-/** Object data for objects inside route:blogs */
-export type BlogsObjectData = {
-    title:string
-    slug:string
-    content:any
-    _id:import("mongodb").ObjectId
-    _status:import("$Types").StatusData
-    _createdAt:number
-    _updatedAt:number
-    _categories:import("$Types").CategoryData[]
-    _tags:import("$Types").TagData[]
-}
-
 /** Object data for objects inside route:posts */
 export type PostsObjectData = {
     title:string
+    slug:string
+    images:{
+        _id:string
+        name:string
+        path:string
+        type:string
+        extension:string
+    }[]
+    content:any
+    thumbnail:{
+        _id:string
+        name:string
+        path:string
+        type:string
+        extension:string
+    }
     _id:import("mongodb").ObjectId
     _status:import("$Types").StatusData
     _createdAt:number
