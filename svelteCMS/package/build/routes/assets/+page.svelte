0cb5f8cc5c261cb43836af2bdd7ccffe43bdc0ac
Anthony <anthonytoribio809@gmail.com>
2023-02-10 18:54:38 -0500

    VERSION 0.16.0

--- svelteCMS/package/build/routes/assets/+page.svelte
+++ svelteCMS/package/build/routes/assets/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
     const API_PATH = "/admin/api/assets"
     export let data:PageServerData
-    ASSETS.set(data.assets)
     import type { PageServerData } from "./$types"
-    import type { AssetData } from "$Types";
-    import type { FetchAssetsLoad,FetchAssetsRes } from "$Types/cms";
+    import type { FetchAssetsLoad,FetchAssetsRes,AssetData } from "$Types";
+    import { page } from "$app/stores"
     import svelteCMS from "$svelteCMS";
     import SvelteHead from "@anthony809/svelte-head"
-    import { ASSETS } from "$Stores"
     import { wait,fetchPost } from "$Utilities"
     // Icons
     import PlusIcon from "$Icons/Plus.svelte";
@@ -17,53 +15,52 @@
     import Assets from "$Comps/shared/assets/Assets.svelte"
     import Button from "$Comps/Button.svelte"
     import NoResult from "$Comps/NoResult.svelte";
-    /** Handle file selected from file uploader */
-    async function handleFileSelect(e:any) {
-        const selectedAsset:AssetData = e.detail
-        // Check if selected assets exists in assets list
-        const assetInAssets = assets.find(data=>data._id===selectedAsset._id)
-        // Add selected asset to assets list
-        if(!assetInAssets){
-            ASSETS.set([...$ASSETS,selectedAsset])
-            assets = [...assets,selectedAsset]
-        }
-    }
     /** Load more assets */
     async function loadMoreAssets() {
         // Set loading more assets
-        isGettingMoreAssets = true
+        loading = true
         // Update page number
         pageNumber = pageNumber+1
         // Send api request
-        const apiLoad:FetchAssetsLoad = { filter:null, count:svelteCMS.config.assetsPerPage,pageNumber }
+        const query = $page.url.searchParams.get("q")
+        const filter = query ? { name:query } : null
+        const apiLoad:FetchAssetsLoad = { filter:filter, count:svelteCMS.config.assetsPerPage,pageNumber }
         const apiResponse:FetchAssetsRes = await fetchPost("PATCH",API_PATH,apiLoad) 
         if(apiResponse.length>0){
             if(apiResponse.length<svelteCMS.config.assetsPerPage) resetStages()
             // Wait 500 milliseconds
             await wait(500)
             // Marge assets with response assets
-            ASSETS.set([...$ASSETS,...apiResponse])
+            assets = [...assets,...apiResponse]
         }
         // Reset stages
         else await resetStages()
         // Remove loading more assets
-        isGettingMoreAssets = false
+        loading = false
     }
     /** Reset stages */
-    async function resetStages(){
+    async function resetStages(W:boolean=true){
         // Wait 500 milliseconds
-        await wait(500)
+        if(W) await wait(500)
         showLoadMoreBtn = false
         pageNumber = 1
     }
     // Variables 
     /** Indicate if file uploader is open or not */
     let isFileUploaderOpen:boolean = false
-    let assets = data.assets
-    let showLoadMoreBtn:boolean = $ASSETS.length >= svelteCMS.config.assetsPerPage
+    let showLoadMoreBtn:boolean = data.assets.length >= svelteCMS.config.assetsPerPage
     let pageNumber = 1
     /** Indicate when loading more assets */
-    let isGettingMoreAssets = false
+    let loading = false
+    $: assets = data.assets
+    $: query = $page.url.searchParams.get("q")
+    $: title = query ? `Result for: ${query}` : "Assets"
+    // Reset stages when page load or change path
+    $: if(data.assets){
+        isFileUploaderOpen = false
+        pageNumber = 1
+        showLoadMoreBtn = data.assets.length >= svelteCMS.config.assetsPerPage
+    }
     const pageData = {
         appName:svelteCMS.site.name,
         favicon:svelteCMS.site.favicon,
@@ -74,12 +71,12 @@
 </script>
 
 <SvelteHead {...pageData}/>
-<FileUploader allowSelection={false} bind:open={isFileUploaderOpen} on:select={handleFileSelect}/>
-{#if $ASSETS.length > 0}
-    <TitleButton title="All Assets" on:click={()=>isFileUploaderOpen=true} icon={PlusIcon}/>
-    <Assets assets={$ASSETS}/>
+<FileUploader allowSelection={false} bind:open={isFileUploaderOpen} on:select={()=>location.reload()}/>
+{#if assets.length > 0}
+    <TitleButton {title} on:click={()=>isFileUploaderOpen=true} icon={PlusIcon}/>
+    <Assets {assets}/>
     {#if showLoadMoreBtn}
-        <Button loading={isGettingMoreAssets} text="Load more" centerBtn={true} --width="fit-content" on:click={loadMoreAssets}/>
+        <Button {loading} text="Load more" centerBtn={true} --width="fit-content" on:click={loadMoreAssets}/>
     {/if}
 {:else}
     <NoResult title="No assets" subTitle="Please some assets" href="/admin/assets" hrefText="Add assets" on:click={()=>isFileUploaderOpen=true}/>
