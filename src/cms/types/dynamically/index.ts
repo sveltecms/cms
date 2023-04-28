85d1378a46c55d0a4e55523a1994c4983b397b5c
Anthony <anthonytoribio809@gmail.com>
2023-04-28 15:41:53 -0400

    CSS bug fixes

--- src/cms/types/dynamically/index.ts
+++ src/cms/types/dynamically/index.ts
@@ -3,7 +3,16 @@ export type PagesData = {
     _id:any
     title:string
     slug:string
-    updatedAt:Date
     content:import("cms/packages/editor/types").EditorJsData
 }
 //TYPE:PagesData:end
+
+//TYPE:TagsData:start
+export type TagsData = {
+    _id:any
+    title:string
+    slug:string
+    description:string
+    image:import("cms/types").AssetData
+}
+//TYPE:TagsData:end
--- src/cms/types/dynamically/projection.ts
+++ src/cms/types/dynamically/projection.ts
@@ -2,7 +2,15 @@
 export type PagesProjection = {
     title?:boolean
     slug?:boolean
-    updatedAt?:boolean
     content?:{[key:string]:boolean | {[key:string]:any}} | boolean
 }
 //PROJECTION:PagesProjection:end
+
+//PROJECTION:TagsData:start
+export type TagsProjection = {
+    title?:boolean
+    slug?:boolean
+    description?:boolean
+    image?:{[key:string]:boolean | {[key:string]:any}} | boolean
+}
+//PROJECTION:TagsProjection:end
